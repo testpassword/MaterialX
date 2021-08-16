cab4ce0effb5abef7cd428c6e4f65c6a882cfb1c
DESKTOP-U5RC1D8\home <bernard.kwok@autodesk.com>
2021-08-16 01:02:12 -0400

    Add some logic.

--- source/MaterialXRender/TinyGLTFLoader.cpp
+++ source/MaterialXRender/TinyGLTFLoader.cpp
@@ -41,6 +41,29 @@ using VertexIndexMap = std::unordered_map<VertexVector, uint32_t, VertexVector::
 //
 // TinyGLTFLoader methods
 //
+#define GBUFFER_OFFSET(i) ((char *)NULL + (i))
+
+#if 0
+static size_t SIZEOF_COMPONENT(int type) {
+    switch (type) {
+    case TINYGLTF_COMPONENT_TYPE_UNSIGNED_BYTE:
+    case TINYGLTF_COMPONENT_TYPE_BYTE:
+        return sizeof(char);
+    case TINYGLTF_COMPONENT_TYPE_UNSIGNED_SHORT:
+    case TINYGLTF_COMPONENT_TYPE_SHORT:
+        return sizeof(short);
+    case TINYGLTF_COMPONENT_TYPE_UNSIGNED_INT:
+    case TINYGLTF_COMPONENT_TYPE_INT:
+        return sizeof(int);
+    case TINYGLTF_COMPONENT_TYPE_FLOAT:
+        return sizeof(float);
+    case TINYGLTF_COMPONENT_TYPE_DOUBLE:
+        return sizeof(double);
+    default:
+        return 0;
+    }
+}
+#endif
 
 bool TinyGLTFLoader::load(const FilePath& filePath, MeshList& meshList)
 {
@@ -89,52 +112,109 @@ bool TinyGLTFLoader::load(const FilePath& filePath, MeshList& meshList)
         mesh->setSourceUri(filePath);
 
         MeshStreamPtr positionStream = MeshStream::create("i_" + MeshStream::POSITION_ATTRIBUTE, MeshStream::POSITION_ATTRIBUTE, 0);
-        MeshStreamPtr normalStream = MeshStream::create("i_" + MeshStream::NORMAL_ATTRIBUTE, MeshStream::NORMAL_ATTRIBUTE, 0);
+        MeshStreamPtr normalStream = nullptr;
         MeshStreamPtr texcoordStream = MeshStream::create("i_" + MeshStream::TEXCOORD_ATTRIBUTE + "_0", MeshStream::TEXCOORD_ATTRIBUTE, 0);
-        texcoordStream->setStride(MeshStream::STRIDE_2D);
+        MeshStreamPtr tangentStream = nullptr;
 
         // Scane for primitives on the mesh
         for (auto gPrim : gMesh.primitives)
         {
             // Get index accessor for the prim. Create a partition
-            tinygltf::Accessor gPrimIndices = model.accessors[gPrim.indices];
-            MeshPartitionPtr part = MeshPartition::create();
-            //part->setIdentifier();
-            //part->setFaceCount(faceCount);
-            mesh->addPartition(part);
+            // Only support triangle indexing
+            if ((gPrim.indices >= 0) &&
+                (gPrim.mode == TINYGLTF_MODE_TRIANGLES))
+            {
+                const tinygltf::Accessor& gaccessor = model.accessors[gPrim.indices];
+                const tinygltf::BufferView& gBufferView = model.bufferViews[gaccessor.bufferView];
+                const tinygltf::Buffer& gBuffer = model.buffers[gBufferView.buffer];
 
-            //MeshIndexBuffer& indices = part->getIndices();
-            //indices.resize(indexCount);
+                size_t indexCount = gaccessor.count;
+                MeshPartitionPtr part = MeshPartition::create();
+                size_t faceCount = indexCount / FACE_VERTEX_COUNT;
+                part->setFaceCount(faceCount);
+                //part->setIdentifier(); - What should this be called ?
+                MeshIndexBuffer& indices = part->getIndices();
+                indices.resize(indexCount);
+                size_t startLocation = gaccessor.byteOffset;
+                size_t bytStride = gaccessor.ByteStride(gBufferView);
+                gaccessor.componentType;
+                for (size_t i = 0; i < indexCount; i++)
+                {
+                    indices[i] = gBuffer.data[startLocation+(i*bytStride)];
+                }
+                mesh->addPartition(part);
+            }
 
             // Get attributes. Note that bufferViews contain the content descriptioon
             for (auto& gattrib : gPrim.attributes)
             {
                 // Find out the bytstride
-                tinygltf::Accessor accessor = model.accessors[gattrib.second];
-                int byteStride = accessor.ByteStride(model.bufferViews[accessor.bufferView]);
-                if (byteStride > -1)
+                const tinygltf::Accessor& accessor = model.accessors[gattrib.second];
+                const tinygltf::BufferView& gBufferView = model.bufferViews[accessor.bufferView];
+                const tinygltf::Buffer& gBuffer = model.buffers[gBufferView.buffer];
+                //int byteStride = accessor.ByteStride(gBufferView);
+                //if (byteStride > -1)
+                unsigned int vectorSize = 3;
+                if (accessor.type == TINYGLTF_TYPE_VEC2)
                 {
-                    if (gattrib.first.compare("POSITION") == 0)
+                    vectorSize = 2;
+                }
+                else if (accessor.type == TINYGLTF_TYPE_VEC4)
+                {
+                    vectorSize = 4;
+                }
+
+                if (gattrib.first.compare("POSITION") == 0)
+                {
+                    // Fill in position stream - TODO
+                    MeshFloatBuffer& buffer = positionStream->getData();
+                    buffer.resize(accessor.count);
+                    memcpy(&buffer[0], &gBuffer.data[0], gBuffer.data.size());
+                }
+                else if (gattrib.first.compare("NORMAL") == 0)
+                {
+                    normalStream = MeshStream::create("i_" + MeshStream::NORMAL_ATTRIBUTE, MeshStream::NORMAL_ATTRIBUTE, 0);
+
+                    // Fill in normal stream - TODO
+                    MeshFloatBuffer& buffer = normalStream->getData();
+                    buffer.resize(accessor.count);
+                    memcpy(&buffer[0], &gBuffer.data[0], gBuffer.data.size());
+                }
+                else if (gattrib.first.compare("TEXCOORD_0") == 0)
+                {
+                    if (vectorSize == 2)
                     {
-                        // Fill in position stream
+                        texcoordStream->setStride(MeshStream::STRIDE_2D);
                     }
-                    else if (gattrib.first.compare("NORMAL") == 0)
-                    {
-                        // Fill in normal stream
-                    }
-                    else if (gattrib.first.compare("TEXCOORD_0") == 0)
-                    {
-                        // Fill in texture coordinate stream
-                    }
+
+                    // Fill in texture coordinate stream - TODO
+                    MeshFloatBuffer& buffer = texcoordStream->getData();
+                    buffer.resize(accessor.count);
+                    memcpy(&buffer[0], &gBuffer.data[0], gBuffer.data.size());
+                }
+                else if (gattrib.first.compare("TANGENT") == 0)
+                {
+                    tangentStream = MeshStream::create("i_" + MeshStream::TANGENT_ATTRIBUTE, MeshStream::NORMAL_ATTRIBUTE, 0);
+
+                    // Fill in tangent stream - TODO
+                    MeshFloatBuffer& buffer = tangentStream->getData();
+                    buffer.resize(accessor.count);
+                    memcpy(&buffer[0], &gBuffer.data[0], gBuffer.data.size());
                 }
             }
         }
 
-        Vector3 boxMin = { MAX_FLOAT, MAX_FLOAT, MAX_FLOAT };
-        Vector3 boxMax = { -MAX_FLOAT, -MAX_FLOAT, -MAX_FLOAT };
+        // General noramsl if none provided
+        if (!normalStream)
+        {
+            normalStream = mesh->generateNormals(positionStream);
+        }
 
-        // Generate tangents.
-        MeshStreamPtr tangentStream = mesh->generateTangents(positionStream, normalStream, texcoordStream);
+        // Generate tangents if none provided
+        if (!tangentStream)
+        {
+            tangentStream = mesh->generateTangents(positionStream, normalStream, texcoordStream);
+        }
 
         // Assign streams to mesh.
         mesh->addStream(positionStream);
@@ -144,10 +224,18 @@ bool TinyGLTFLoader::load(const FilePath& filePath, MeshList& meshList)
         {
             mesh->addStream(tangentStream);
         }
+
+        // Assign properties to mesh.
+        mesh->setVertexCount(positionStream->getData().size() / MeshStream::STRIDE_3D);
+        Vector3 boxMin = { MAX_FLOAT, MAX_FLOAT, MAX_FLOAT };
+        Vector3 boxMax = { -MAX_FLOAT, -MAX_FLOAT, -MAX_FLOAT };
+        mesh->setMinimumBounds(boxMin);
+        mesh->setMaximumBounds(boxMax);
+        Vector3 sphereCenter = (boxMax + boxMin) * 0.5;
+        mesh->setSphereCenter(sphereCenter);
+        mesh->setSphereRadius((sphereCenter - boxMin).getMagnitude());
     }
-
-
-	return false;
+	return true;
 #if 0    
  
  
@@ -226,28 +314,7 @@ bool TinyGLTFLoader::load(const FilePath& filePath, MeshList& meshList)
     if (!normalsFound)
     {
         normalStream = mesh->generateNormals(positionStream);
-    }
-
-    // Generate tangents.
-    MeshStreamPtr tangentStream = mesh->generateTangents(positionStream, normalStream, texcoordStream);
-
-    // Assign streams to mesh.
-    mesh->addStream(positionStream);
-    mesh->addStream(normalStream);
-    mesh->addStream(texcoordStream);
-    if (tangentStream)
-    {
-        mesh->addStream(tangentStream);
-    }
-
-    // Assign properties to mesh.
-    mesh->setVertexCount(positionStream->getData().size() / MeshStream::STRIDE_3D);
-    mesh->setMinimumBounds(boxMin);
-    mesh->setMaximumBounds(boxMax);
-    Vector3 sphereCenter = (boxMax + boxMin) * 0.5;
-    mesh->setSphereCenter(sphereCenter);
-    mesh->setSphereRadius((sphereCenter - boxMin).getMagnitude());
-    return true;
+    }   
 #endif
 }
 
