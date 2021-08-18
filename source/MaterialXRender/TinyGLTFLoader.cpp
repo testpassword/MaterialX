c5643f427d2b2491b181dcaa0723971c1d3506e4
DESKTOP-U5RC1D8\home <bernard.kwok@autodesk.com>
2021-08-18 00:00:20 -0400

    test.

--- source/MaterialXRender/TinyGLTFLoader.cpp
+++ source/MaterialXRender/TinyGLTFLoader.cpp
@@ -43,8 +43,52 @@ using VertexIndexMap = std::unordered_map<VertexVector, uint32_t, VertexVector::
 //
 #define GBUFFER_OFFSET(i) ((char *)NULL + (i))
 
-#if 0
-static size_t SIZEOF_COMPONENT(int type) {
+
+uint32_t VALUE_AS_UINT32(int type, const unsigned char* value)
+{
+	switch (type) 
+    {
+	    case TINYGLTF_COMPONENT_TYPE_UNSIGNED_BYTE:
+	    case TINYGLTF_COMPONENT_TYPE_BYTE:
+		    return static_cast<uint32_t>(*value);
+	    case TINYGLTF_COMPONENT_TYPE_UNSIGNED_SHORT:
+	    case TINYGLTF_COMPONENT_TYPE_SHORT:
+		    return static_cast<uint32_t>(*(short*)value);
+	    case TINYGLTF_COMPONENT_TYPE_UNSIGNED_INT:
+	    case TINYGLTF_COMPONENT_TYPE_INT:
+		    return static_cast<uint32_t>(*(int*)value);
+	    case TINYGLTF_COMPONENT_TYPE_FLOAT:
+		    return static_cast<uint32_t>(*(int*)value);
+	    case TINYGLTF_COMPONENT_TYPE_DOUBLE:
+		    return static_cast<uint32_t>(*(double*)value);
+	    default:
+		    return 0;
+	}
+}
+
+float VALUE_AS_FLOAT(int type, const unsigned char* value)
+{
+    switch (type)
+    {
+    case TINYGLTF_COMPONENT_TYPE_UNSIGNED_BYTE:
+    case TINYGLTF_COMPONENT_TYPE_BYTE:
+        return static_cast<float>(*value);
+    case TINYGLTF_COMPONENT_TYPE_UNSIGNED_SHORT:
+    case TINYGLTF_COMPONENT_TYPE_SHORT:
+        return static_cast<float>(*(short*)value);
+    case TINYGLTF_COMPONENT_TYPE_UNSIGNED_INT:
+    case TINYGLTF_COMPONENT_TYPE_INT:
+        return static_cast<float>(*(int*)value);
+    case TINYGLTF_COMPONENT_TYPE_FLOAT:
+        return static_cast<float>(*(int*)value);
+    case TINYGLTF_COMPONENT_TYPE_DOUBLE:
+        return static_cast<float>(*(double*)value);
+    default:
+        return 0;
+    }
+}
+
+size_t ComponentTypeByteSize(int type) {
     switch (type) {
     case TINYGLTF_COMPONENT_TYPE_UNSIGNED_BYTE:
     case TINYGLTF_COMPONENT_TYPE_BYTE:
@@ -63,7 +107,7 @@ static size_t SIZEOF_COMPONENT(int type) {
         return 0;
     }
 }
-#endif
+
 
 bool TinyGLTFLoader::load(const FilePath& filePath, MeshList& meshList)
 {
@@ -117,14 +161,15 @@ bool TinyGLTFLoader::load(const FilePath& filePath, MeshList& meshList)
         MeshStreamPtr tangentStream = nullptr;
 
         // Scane for primitives on the mesh
-        for (auto gPrim : gMesh.primitives)
+        for (tinygltf::Primitive& gPrim : gMesh.primitives)
         {
             // Get index accessor for the prim. Create a partition
             // Only support triangle indexing
-            if ((gPrim.indices >= 0) &&
+            int accessorIndex = gPrim.indices;
+            if ((accessorIndex >= 0) &&
                 (gPrim.mode == TINYGLTF_MODE_TRIANGLES))
             {
-                const tinygltf::Accessor& gaccessor = model.accessors[gPrim.indices];
+                const tinygltf::Accessor& gaccessor = model.accessors[accessorIndex];
                 const tinygltf::BufferView& gBufferView = model.bufferViews[gaccessor.bufferView];
                 const tinygltf::Buffer& gBuffer = model.buffers[gBufferView.buffer];
 
@@ -136,49 +181,104 @@ bool TinyGLTFLoader::load(const FilePath& filePath, MeshList& meshList)
                 MeshIndexBuffer& indices = part->getIndices();
                 indices.resize(indexCount);
                 size_t startLocation = gaccessor.byteOffset;
-                size_t bytStride = gaccessor.ByteStride(gBufferView);
-                gaccessor.componentType;
-                for (size_t i = 0; i < indexCount; i++)
+                size_t byteStride = gaccessor.ByteStride(gBufferView);
+                std::cout << "*** Indexing: {\n";
+				for (size_t i = 0; i < indexCount; i++)
                 {
-                    indices[i] = gBuffer.data[startLocation+(i*bytStride)];
+                    size_t offset = startLocation + (i * byteStride);
+                    uint32_t bufferIndex = VALUE_AS_UINT32(gaccessor.componentType, &(gBuffer.data[offset]) );
+                    indices[i] = bufferIndex;
+                    std::cout << "[" + std::to_string(i) + "] = " + std::to_string(bufferIndex) + "\n";
                 }
+                std::cout << "}\n";
                 mesh->addPartition(part);
             }
 
             // Get attributes. Note that bufferViews contain the content descriptioon
             for (auto& gattrib : gPrim.attributes)
             {
-                // Find out the bytstride
-                const tinygltf::Accessor& accessor = model.accessors[gattrib.second];
-                const tinygltf::BufferView& gBufferView = model.bufferViews[accessor.bufferView];
+                // Find out the byteStride
+                const tinygltf::Accessor& gAccessor = model.accessors[gattrib.second];
+                const tinygltf::BufferView& gBufferView = model.bufferViews[gAccessor.bufferView];
                 const tinygltf::Buffer& gBuffer = model.buffers[gBufferView.buffer];
-                //int byteStride = accessor.ByteStride(gBufferView);
-                //if (byteStride > -1)
+                //size_t startLocation = gAccessor.byteOffset;
+                size_t byteStride = gAccessor.ByteStride(gBufferView);
+                //size_t byteStride = ComponentTypeByteSize(gAccessor.componentType);
+                size_t floatStride = byteStride / sizeof(float);
+                size_t byteOffset = gAccessor.byteOffset;
+                size_t startLocation = byteOffset / sizeof(float);
+
                 unsigned int vectorSize = 3;
-                if (accessor.type == TINYGLTF_TYPE_VEC2)
+                if (gAccessor.type == TINYGLTF_TYPE_VEC2)
                 {
                     vectorSize = 2;
                 }
-                else if (accessor.type == TINYGLTF_TYPE_VEC4)
+                else if (gAccessor.type == TINYGLTF_TYPE_VEC4)
                 {
                     vectorSize = 4;
                 }
 
+                std::cout << "** READ ATTRIB: " << gattrib.first <<  
+                    " from buffer: " << std::to_string(gBufferView.buffer) << std::endl;
+                std::cout << "-- ATTR-START-LOC: " << std::to_string(startLocation) << std::endl;
+                std::cout << "-- ATTR-STRIDE: " << std::to_string(floatStride) << std::endl;
+                std::cout << "-- ATTR-VECSIZE: " << std::to_string(vectorSize) << std::endl;
+
                 if (gattrib.first.compare("POSITION") == 0)
                 {
-                    // Fill in position stream - TODO
+                    // Fill in stream 
                     MeshFloatBuffer& buffer = positionStream->getData();
-                    buffer.resize(accessor.count);
-                    memcpy(&buffer[0], &gBuffer.data[0], gBuffer.data.size());
+
+                    // Resize data and get pointer to data as float. 
+                    size_t dataCount = gAccessor.count;
+                    buffer.resize(dataCount* vectorSize);
+                    float* floatPointer = (float*)&(gBuffer.data[0]);
+                    
+                    // Jump to start of data
+                    floatPointer += startLocation;
+                    std::cout << "{\n";
+                    for (size_t i = 0; i < dataCount; i++)
+                    {
+                        // Jump to start of vector
+                        floatPointer += i * floatStride;
+                        for (size_t v = 0; v < vectorSize; v++)
+                        {
+                            // Jump to component in vector and save
+                            float bufferData = *(floatPointer + v);
+                            buffer[i] = bufferData;
+                            std::cout << "[" + std::to_string(i) + "] = " + std::to_string(bufferData) + "\n";
+                        }
+                    }
+                    std::cout << "}\n";
                 }
                 else if (gattrib.first.compare("NORMAL") == 0)
                 {
                     normalStream = MeshStream::create("i_" + MeshStream::NORMAL_ATTRIBUTE, MeshStream::NORMAL_ATTRIBUTE, 0);
 
-                    // Fill in normal stream - TODO
+                    // Fill in normal stream 
                     MeshFloatBuffer& buffer = normalStream->getData();
-                    buffer.resize(accessor.count);
-                    memcpy(&buffer[0], &gBuffer.data[0], gBuffer.data.size());
+
+                    // Resize data and get pointer to data as float. 
+                    size_t dataCount = gAccessor.count;
+                    buffer.resize(dataCount * vectorSize);
+                    float* floatPointer = (float*)&(gBuffer.data[0]);
+
+                    // Jump to start of data
+                    floatPointer += startLocation;
+                    std::cout << "{\n";
+                    for (size_t i = 0; i < dataCount; i++)
+                    {
+                        // Jump to start of vector
+                        floatPointer += i * floatStride;
+                        for (size_t v = 0; v < vectorSize; v++)
+                        {
+                            // Jump to component in vector and save
+                            float bufferData = *(floatPointer + v);
+                            buffer[i] = bufferData;
+                            std::cout << "[" + std::to_string(i) + "] = " + std::to_string(bufferData) + "\n";
+                        }
+                    }
+                    std::cout << "}\n";
                 }
                 else if (gattrib.first.compare("TEXCOORD_0") == 0)
                 {
@@ -187,19 +287,61 @@ bool TinyGLTFLoader::load(const FilePath& filePath, MeshList& meshList)
                         texcoordStream->setStride(MeshStream::STRIDE_2D);
                     }
 
-                    // Fill in texture coordinate stream - TODO
+                    // Fill in texture coordinate stream 
                     MeshFloatBuffer& buffer = texcoordStream->getData();
-                    buffer.resize(accessor.count);
-                    memcpy(&buffer[0], &gBuffer.data[0], gBuffer.data.size());
+
+                    // Resize data and get pointer to data as float. 
+                    size_t dataCount = gAccessor.count;
+                    buffer.resize(dataCount* vectorSize);
+                    float* floatPointer = (float*)&(gBuffer.data[0]);
+
+                    // Jump to start of data
+                    floatPointer += startLocation;
+                    std::cout << "{\n";
+                    for (size_t i = 0; i < dataCount; i++)
+                    {
+                        // Jump to start of vector
+                        floatPointer += i * floatStride;
+                        for (size_t v = 0; v < vectorSize; v++)
+                        {
+                            // Jump to component in vector and save
+                            float bufferData = *(floatPointer + v);
+                            buffer[i] = bufferData;
+                            std::cout << "[" + std::to_string(i) + "][" + std::to_string(v) +
+                                "] = " + std::to_string(bufferData) + "\n";
+                        }
+                    }
+                    std::cout << "}\n";
                 }
                 else if (gattrib.first.compare("TANGENT") == 0)
                 {
                     tangentStream = MeshStream::create("i_" + MeshStream::TANGENT_ATTRIBUTE, MeshStream::NORMAL_ATTRIBUTE, 0);
 
-                    // Fill in tangent stream - TODO
+                    // Fill in tangent stream 
                     MeshFloatBuffer& buffer = tangentStream->getData();
-                    buffer.resize(accessor.count);
-                    memcpy(&buffer[0], &gBuffer.data[0], gBuffer.data.size());
+
+                    // Resize data and get pointer to data as float. 
+                    size_t dataCount = gAccessor.count;
+                    buffer.resize(dataCount * vectorSize);
+                    float* floatPointer = (float*)&(gBuffer.data[0]);
+
+                    // Jump to start of data
+                    floatPointer += startLocation;
+                    std::cout << "{\n";
+                    for (size_t i = 0; i < dataCount; i++)
+                    {
+                        // Jump to start of vector
+                        floatPointer += i * floatStride;
+                        for (size_t v = 0; v < vectorSize; v++)
+                        {
+                            // Jump to component in vector and save
+                            float bufferData = *(floatPointer + v);
+                            buffer[i] = bufferData;
+                            std::cout << "[" + std::to_string(i) + "]["+ std::to_string(v) +
+                                "] = " + std::to_string(bufferData) + "\n";
+                        }
+                    }
+                    std::cout << "}\n";
                 }
             }
         }
